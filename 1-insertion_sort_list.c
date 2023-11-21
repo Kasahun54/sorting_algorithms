8d9dbe86726a0754368b203dd6d81383760b6e3a
Kassahun Shimelis <kassahun.shimelis54@gmail.com>
2023-11-21 11:45:16 -0800

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,9 +1,56 @@
 #include "sort.h"
+
 /**
-  * insertion_sort_list - doubly linked list insertion sort
-  * @list: point to the head of linked list
-  * 
-  * Return: Nothing
+  * dlistint_len - count number of nodes of a doubly linked list.
+  * @h: pointer to first node.
+  * Return: number of nodes.
+  */
+size_t dlistint_len(const listint_t *h)
+{
+	listint_t *tmp;
+	size_t count = 0;
+
+	if (h == NULL)
+		return (count);
+	tmp = (listint_t *)h;
+	while (tmp != NULL)
+	{
+		tmp = tmp->next;
+		count++;
+	}
+	return (count);
+}
+
+/**
+  * swap - swap two nodes of a linked list.
+  * @head: address of pointer to head of list.
+  * @cursor1: node first node.
+  * @cursor2: node previous to @cursor1.
+  * Return: pointer to first node.
+  */
+listint_t *swap(listint_t **head, listint_t *cursor1, listint_t *cursor2)
+{
+	listint_t *c1_next, *c2_prev;
+
+	c1_next = cursor1->next;
+	c2_prev = cursor2->prev;
+	if (*head == cursor2)
+		*head = cursor1;
+	else
+		c2_prev->next = cursor1;
+	cursor1->next = cursor2;
+	cursor1->prev = c2_prev;
+	if (c1_next != NULL)
+		c1_next->prev = cursor2;
+	cursor2->next = c1_next;
+	cursor2->prev = cursor1;
+	return (cursor1);
+}
+
+/**
+  * insertion_sort_list - sort a doubly linked list of integers using
+  * insertion sort.
+  * @list: address of pointer to head of linked list.
   */
 void insertion_sort_list(listint_t **list)
 {
@@ -29,53 +76,4 @@ void insertion_sort_list(listint_t **list)
 		}
 		cursor = cursor->next;
 	}
-}
-
-/**
-  * dlistint_len - count nodes in doubly linked list
-  * @h: point to the first node
-  * 
-  * Return: number of nodes
-  */
-size_t dlistint_len(const listint_t *h)
-{
-	listint_t *tmp;
-	size_t count = 0;
-
-	if (h == NULL)
-		return (count);
-	tmp = (listint_t *)h;
-	while (tmp != NULL)
-	{
-		tmp = tmp->next;
-		count++;
-	}
-	return (count);
-}
-
-/**
-  * swap - exchange nodes in a linked list
-  * @head: address of pointer
-  * @cursor1: first node
-  * @cursor2: previous to first node
-  * 
-  * Return: pointer to the first node
-  */
-listint_t *swap(listint_t **head, listint_t *cursor1, listint_t *cursor2)
-{
-	listint_t *c1_next, *c2_prev;
-
-	c1_next = cursor1->next;
-	c2_prev = cursor2->prev;
-	if (*head == cursor2)
-		*head = cursor1;
-	else
-		c2_prev->next = cursor1;
-	cursor1->next = cursor2;
-	cursor1->prev = c2_prev;
-	if (c1_next != NULL)
-		c1_next->prev = cursor2;
-	cursor2->next = c1_next;
-	cursor2->prev = cursor1;
-	return (cursor1);
 }
